8936a5795db80f763357b19a5e75fd332a0c2d15
HeyStenson <h.n.stenson@gmail.com>
2016-02-22 07:31:31 -0800

    fixed the jquery call on site.js

--- js/site.js
+++ js/site.js
@@ -1,3 +1,5 @@
+console.log("Oh, cool, you're looking at my code! Definitely let me know what you think. I love learning new things about how to write better code, optimize user experience, and link to more cat pictures.");
+
 // currently active menu item 
 var current_item = 0;
 
@@ -5,17 +7,15 @@ var current_item = 0;
 var section_hide_time = 1300;
 var section_show_time = 1300;
 
-jQuery(document).ready(function($) {
-
+$(function() {
 	// Switch section
-	$('a', '.mainmenu').click(function() 
-	{
+	$('a', '.mainmenu').click(function() {
 		if(!$(this).hasClass('active')) { 
 			current_item = this;
 			// close all visible divs with the class of .section
-			$('.section:visible').fadeOut( section_hide_time, function() { 
+			$('.section:visible').fadeOut(section_hide_time, function() { 
 				$('a', '.mainmenu').removeClass('active');  
-				$(current_item).addClass( 'active' );
+				$(current_item).addClass('active');
 				var new_section = $( $(current_item).attr('href') );
 				new_section.fadeIn(section_show_time);
 			});
